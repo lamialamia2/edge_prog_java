d36ab6f81edfb14df50930d578001c2befa3186f
Lamia Akter <109574639+lamialamia2@users.noreply.github.com>
2025-06-01 12:43:06 -0400

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,10 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else {
+            System.out.println("Negative or Zero");
+        }
+    }
+}
