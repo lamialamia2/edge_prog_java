0a5f8380626d1178d806d9dc013a6b536f9e2bf5
Lamia Akter <109574639+lamialamia2@users.noreply.github.com>
2025-06-01 12:44:40 -0400

    Create array.java

---
+++ array.java
@@ -0,0 +1,8 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30};
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
